12d46b92efa056dd616d5b133d42bf0819ae6916
Jayashre <jaya2004kra@gmail.com>
2024-11-20 12:44:28 +0530

    added fonts

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,32 @@
 import "../app/globals.css"
+import {Poppins, Urbanist, Kumbh_Sans} from "next/font/google";
+
+const poppins_init = Poppins({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-poppins',
+});
+
+const urbanist_init = Urbanist({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-urbanist',
+});
+
+const kumbh_sans_init = Kumbh_Sans({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-kumbh_sans',
+});
 
 export const metadata = {
   title: "EcoChain",
-  description: "A smart, AI-driven solution designed  to optimize logistics and waste management for  SMBs, leveraging Amazon's powerful  infrastructure to promote sustainability while  maximizing efficiency.",
+  description: "Empowering SMBs with sustainable logistics solutions.",
 };
 
 function MyApp({ Component, pageProps }) {
     return (
-        <div className="antialiased">
+        <div className={`${urbanist_init.variable} ${kumbh_sans_init.variable} ${poppins_init.variable}`}>
           <main>
             <Component {...pageProps} />
           </main>
