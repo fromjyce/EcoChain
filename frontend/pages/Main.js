977cdc457827b931b9425461b43eb31e81cc64a8
Jayashre <jaya2004kra@gmail.com>
2024-11-20 10:56:06 +0530

    added

---
+++ frontend/pages/Main.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Main() {
+  return (
+    <>
+    <Head>
+        <title>EcoChain</title>
+        <meta name="description" content="EcoChain" />
+    </Head>
+   <h1>Hi</h1>
+   </>
+  );
+}
