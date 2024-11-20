e0b9b954d8da7c16368894e6ba353c831ccbdf9d
Jayashre <jaya2004kra@gmail.com>
2024-11-20 13:55:36 +0530

    added footer content

---
+++ frontend/pages/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+        <footer className="bg-[#102409] text-white py-2 poppins">
+          <div className="container mx-auto text-center">
+            <p>Developed by <strong>Team Pravah</strong> for <strong>Amazon Smbhav Hacakthon 2024</strong></p>
+          </div>
+        </footer>
+  );
+}
