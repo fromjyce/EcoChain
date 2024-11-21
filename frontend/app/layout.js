fe0b2dd66381043266321317b3ead6a3f674693c
Jayashre <jaya2004kra@gmail.com>
2024-11-22 00:41:18 +0530

    added fonts - kanit

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import {Poppins, Urbanist, Kumbh_Sans} from "next/font/google";
+import {Poppins, Urbanist, Kumbh_Sans, Kanit} from "next/font/google";
 
 const poppins_init = Poppins({
   subsets: ['latin'],
@@ -19,6 +19,12 @@ const kumbh_sans_init = Kumbh_Sans({
   variable: '--font-kumbh_sans',
 });
 
+const kanit_init = Kanit({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-kanit',
+});
+
 export const metadata = {
   title: "EcoChain",
   description: "Empowering SMBs with sustainable logistics solutions.",
@@ -28,7 +34,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${urbanist_init.variable} ${kumbh_sans_init.variable} ${poppins_init.variable}`}
+        className={`${urbanist_init.variable} ${kumbh_sans_init.variable} ${poppins_init.variable} ${kanit_init.variable}`}
       >
         {children}
       </body>
