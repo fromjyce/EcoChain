7fd4c9c4b35c1b58501cf008669576b84f09cfd1
Jayashre <jaya2004kra@gmail.com>
2024-11-22 00:57:26 +0530

    mod gap and font size

--- frontend/pages/dashboard/amascore.js
+++ frontend/pages/dashboard/amascore.js
@@ -8,16 +8,16 @@ const GreenScoreWidget = ({ todayScore, previousDayScore, nextDayScore }) => {
   nextDay.setDate(today.getDate() + 1);
 
   return (
-    <div className="p-3 bg-[#5ea07b] rounded shadow">
+    <div className="p-3 bg-[#6eaa88] rounded shadow">
       <h2 className="text-2xl font-bold urbanist text-[#42282c] mb-1 text-center">
         Amazon Green Score
       </h2>
-      <p className="text-sm urbanist text-white text-center mb-2">
+      <p className="text-base urbanist text-[#f4f4f4] text-center mb-2">
         The <strong>Amazon Green Score</strong> tracks the environmental sustainability of your 
         e-commerce logistics by evaluating metrics like carbon footprint, 
         reusable packaging, and eco-conscious delivery options.
       </p>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-3">
         <GSWidget score={previousDayScore} date={previousDay.toISOString()} />
         <GSWidget score={todayScore} date={today.toISOString()} />
         <GSWidget score={nextDayScore} date={nextDay.toISOString()} />
