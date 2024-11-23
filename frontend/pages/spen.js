8a7d4823aee32d9c1082411fabd725cf9f60da49
Jayashre <jaya2004kra@gmail.com>
2024-11-24 01:33:57 +0530

    added spen template

---
+++ frontend/pages/spen.js
@@ -0,0 +1,40 @@
+"use client";
+import Sidebar from './components/sidebar';
+import Head from "next/head";
+import { useEffect, useState } from 'react';
+import Navbar from './components/navbar';
+
+
+const handleLogout = () => {
+    console.log("User logged out");
+  };
+
+const SPEN = () => {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+  return (
+    <>
+    <Head>
+        <title>EcoChain | Packaging Exchange</title>
+        <meta name="description" content="Empowering SMBs with sustainable logistics solutions." />
+      </Head>
+      <div className='content'>
+      <div className="flex">
+        <Sidebar />
+        <div className="bg-[#fefee3ff] flex-1">
+        <Navbar smbName="EcoDelight Crafts" logoPath="/dummy/smblogo.png" onLogout={handleLogout} />
+        <div className='p-6'>
+          <h1 className="text-5xl font-bold text-[#102409] mb-6 urbanist">Manage Your Packages.</h1>
+          
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default SPEN;
