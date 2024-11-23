6c224bf669991e9d1ea504b6ae7a03a103cff075
Jayashre <jaya2004kra@gmail.com>
2024-11-24 02:51:51 +0530

    added logout logic

--- frontend/pages/components/navbar.js
+++ frontend/pages/components/navbar.js
@@ -1,28 +1,45 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useAuth } from "@/context/useAuth";
 
-const Navbar = ({ smbName, logoPath}) => {
+const Navbar = ({ smbName, logoPath }) => {
+  const { user, logout } = useAuth();
   const [isClient, setIsClient] = useState(false);
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-    return (
-      <div className="navbar">
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <div className="navbar">
       <header className="flex items-center justify-between bg-[#2c6e49ff] p-4 text-[#fefee3ff]">
         <div className="flex items-center">
           <img src={logoPath} alt={`${smbName} Logo`} className="h-10 mr-3" />
           <h1 className="text-2xl poppins font-bold">{smbName}</h1>
         </div>
-        <button
-          onClick={onLogout}
-          className="bg-[#F3D946] px-4 py-2 urbanist text-black font-bold rounded hover:bg-[#c2ae38]"
-        >
-          Logout
-        </button>
+
+        {user ? (
+          <button
+            onClick={handleLogout}
+            className="bg-[#F3D946] px-4 py-2 urbanist text-black font-bold rounded hover:bg-[#c2ae38]"
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            onClick={() => router.push('/signin')}
+            className="bg-[#F3D946] px-4 py-2 urbanist text-black font-bold rounded hover:bg-[#c2ae38]"
+          >
+            Sign In
+          </button>
+        )}
       </header>
-      </div>
-    );
-  };
-  
-  export default Navbar;
-  
+    </div>
+  );
+};
+
+export default Navbar;
