7ebc200722eff996fa30cb0b654c01b161844392
Jayashre <jaya2004kra@gmail.com>
2024-11-26 22:52:11 +0530

    added route links

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('route/', include('route_optimization.urls')),
 ]
